169bc50cfffde857e122b4acfa08219d862ae043
Benjamin Hoffmann <benjamin.hoffmann@camunda.com>
2017-08-10 13:16:49 +0200

    add incident count

---
+++ src/main/java/com/camunda/consulting/metrics/GaugeIncidentCounts.java
@@ -0,0 +1,29 @@
+package com.camunda.consulting.metrics;
+
+import com.camunda.consulting.metrics.base.AbstractMetricProvider;
+import io.prometheus.client.Gauge;
+import org.camunda.bpm.engine.ManagementService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GaugeIncidentCounts extends AbstractMetricProvider{
+
+    @Autowired
+    RuntimeService runtimeService;
+
+
+    private static final Gauge openIncidents = Gauge.build()
+            .name("camunda_incidents_open")
+            .help("Number of open incidents.")
+            .register();
+
+
+    @Override
+    public void updateMetrics() {
+
+        openIncidents.set(runtimeService.createIncidentQuery().count());
+
+    }
+}
